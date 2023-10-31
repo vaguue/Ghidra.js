c917ec9d5f34e38f5f356304576d634c5b043726
Seva D <sabrinarempel7@gmail.com>
2023-11-01 01:02:23 +0300

    prompts

--- Extension/Javet/Ghidra.js/build.sh
+++ Extension/Javet/Ghidra.js/build.sh
@@ -4,7 +4,9 @@
 root=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  );
 target=$root/../../../dist/javet
 
+rm $root/dist/* &> /dev/null
 GHIDRA_INSTALL_DIR=/opt/homebrew/Caskroom/ghidra/10.4-20230928/ghidra_10.4_PUBLIC gradle buildExtension -x test
 
 mkdir -p $target
+rm $target/* &> /dev/null
 cp dist/* $target
