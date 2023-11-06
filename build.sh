08f024ddfe9f2160063c19154894f82544c9ac01
vaguue <little_scamp@yahoo.com>
2023-11-06 16:52:26 +0300

    creating actions

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PATH=$PATH:/opt/homebrew/Caskroom/ghidra/10.4-20230928/ghidra_10.4_PUBLIC
+
+buildJavet() {
+  cur=$(pwd)
+  cd Extension/Javet/Ghidra.js
+  ./build.sh $@
+  cd $cur
+}
+
+buildJavet $@
