62c12598b30c8e8eaecd509a7cdffb10f4af706a
Seva D <VSDrozdov@detmir.ru>
2023-11-06 22:17:02 +0300

    tmp

--- Extension/Javet/Ghidra.js/build.sh
+++ Extension/Javet/Ghidra.js/build.sh
@@ -3,7 +3,8 @@
 set -x
 
 javetId=$1
-suffix="_Ghidra.js.zip"
+[ -z "$2" ] && export GHIDRA_INSTALL_DIR=/opt/homebrew/Caskroom/ghidra/10.4-20230928/ghidra_10.4_PUBLIC || export GHIDRA_INSTALL_DIR=$2
+outFile="Ghidra.js.zip"
 
 echo javetId: $javetId
 echo GHIDRA_INSTALL_DIR: $GHIDRA_INSTALL_DIR
@@ -13,8 +14,9 @@ target=$root/../../../dist/javet
 
 rm $root/dist/* &> /dev/null
 gradle buildExtension -x test -PjavetId=$javetId
+[ $? -ne 0 ] && exit 1
 
-mv $root/dist/* $root/dist/$javetId$suffix
+mv $root/dist/* $root/dist/$outFile
 
 mkdir -p $target
 rm $target/* &> /dev/null
