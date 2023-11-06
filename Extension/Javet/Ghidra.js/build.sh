5350560c44c578c61938c1ac0213cc7f86c2c2fa
vaguue <little_scamp@yahoo.com>
2023-11-06 18:59:53 +0300

    tmp

--- Extension/Javet/Ghidra.js/build.sh
+++ Extension/Javet/Ghidra.js/build.sh
@@ -6,6 +6,9 @@ javetId=$0
 [ -z "$1" ] && GHIDRA_INSTALL_DIR=/opt/homebrew/Caskroom/ghidra/10.4-20230928/ghidra_10.4_PUBLIC || GHIDRA_INSTALL_DIR=$1
 suffix="_Ghidra.js.zip"
 
+echo javetId: $javetId
+echo GHIDRA_INSTALL_DIR: $GHIDRA_INSTALL_DIR
+
 root=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}"  )" &> /dev/null && pwd  );
 target=$root/../../../dist/javet
 
