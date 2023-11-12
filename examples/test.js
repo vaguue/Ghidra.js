bc7e65945aa53c2a21b0217c153a4d065b4bf8d3
Seva D <VSDrozdov@detmir.ru>
2023-11-12 04:31:10 +0300

    Adding testing

---
+++ examples/test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+const domainFile = currentProgram.getDomainFile();
+
+assert.equal(currentProgram.getName(), 'yes');
+assert.equal(domainFile.getPathname(), '/yes');
+assert.equal(currentProgram.getExecutableFormat(), 'Mac OS X Mach-O');
+assert.equal(currentProgram.getLanguageID().getIdAsString(), 'AARCH64:LE:64:AppleSilicon');
+assert.equal(currentProgram.getCompilerSpec().getCompilerSpecID().getIdAsString(), 'default');
+assert.equal(1, 2);
+
+console.log('[*] ok');
