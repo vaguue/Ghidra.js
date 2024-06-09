f9eb5efbb522b123900bfd8e423592724903f0ec
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2024-06-09 10:39:27 +0100

    add more types

--- typings/Address.d.ts
+++ typings/Address.d.ts
@@ -1,2 +1,49 @@
+// https://ghidra.re/ghidra_docs/api/ghidra/program/model/address/Address.html
 export interface Address {
+    add(displacement: number): Address;
+    addNoWrap(displacement: number): Address;
+    addNoWrap(displacement: BigInteger): Address;
+    addWrap(displacement: number): Address;
+    addWrapSpace(displacement: number): Address;
+    equals(o: any): boolean;
+    getAddress(addrString: string): Address;
+    getAddressableWordOffset(): number;
+    getAddressSpace(): AddressSpace;
+    getNewAddress(byteOffset: number): Address;
+    getNewAddress(offset: number, isAddressableWordOffset: boolean): Address;
+    getNewTruncatedAddress(offset: number, isAddressableWordOffset: boolean): Address;
+    getOffset(): number;
+    getOffsetAsBigInteger(): BigInteger;
+    getPhysicalAddress(): Address;
+    getPointerSize(): number;
+    getSize(): number;
+    getUnsignedOffset(): number;
+    hashCode(): number;
+    hasSameAddressSpace(addr: Address): boolean;
+    isConstantAddress(): boolean;
+    isExternalAddress(): boolean;
+    isHashAddress(): boolean;
+    isLoadedMemoryAddress(): boolean;
+    isMemoryAddress(): boolean;
+    isNonLoadedMemoryAddress(): boolean;
+    isRegisterAddress(): boolean;
+    isStackAddress(): boolean;
+    isSuccessor(addr: Address): boolean;
+    isUniqueAddress(): boolean;
+    isVariableAddress(): boolean;
+    max(a: Address, b: Address): Address;
+    min(a: Address, b: Address): Address;
+    next(): Address;
+    previous(): Address;
+    subtract(displacement: number): Address;
+    subtract(addr: Address): number;
+    subtractNoWrap(displacement: number): Address;
+    subtractWrap(displacement: number): Address;
+    subtractWrapSpace(displacement: number): Address;
+    toString(): string;
+    toString(showAddressSpace: boolean): string;
+    toString(showAddressSpace: boolean, pad: boolean): string;
+    toString(showAddressSpace: boolean, minNumDigits: number): string;
+    toString(prefix: string): string;
+    //compareTo(addr: Address): number; // inherited from Comparable
 }
--- typings/FunctionManager.d.ts
+++ typings/FunctionManager.d.ts
@@ -1,2 +1,30 @@
+// https://ghidra.re/ghidra_docs/api/ghidra/program/model/listing/FunctionManager.html
 export interface FunctionManager {
+    createFunction(name: string, entryPoint: Address, body: AddressSetView, source: SourceType): Function;
+    createFunction(name: string, nameSpace: Namespace, entryPoint: Address, body: AddressSetView, source: SourceType): Function;
+    createThunkFunction(name: string, nameSpace: Namespace, entryPoint: Address, body: AddressSetView, thunkedFunction: Function, source: SourceType): Function;
+    getCallingConvention(name: string): PrototypeModel;
+    getCallingConventionNames(): string[];
+    getCallingConventions(): PrototypeModel[];
+    getDefaultCallingConvention(): PrototypeModel;
+    getExternalFunctions(): FunctionIterator;
+    getFunction(key: number): Function;
+    getFunctionAt(entryPoint: Address): Function;
+    getFunctionContaining(addr: Address): Function;
+    getFunctionCount(): number;
+    getFunctions(forward: boolean): FunctionIterator;
+    getFunctions(start: Address, forward: boolean): FunctionIterator;
+    getFunctions(asv: AddressSetView, forward: boolean): FunctionIterator;
+    getFunctionsNoStubs(forward: boolean): FunctionIterator;
+    getFunctionsNoStubs(start: Address, forward: boolean): FunctionIterator;
+    getFunctionsNoStubs(asv: AddressSetView, forward: boolean): FunctionIterator;
+    getFunctionsOverlapping(set: AddressSetView): Iterator<Function>;
+    getFunctionTagManager(): FunctionTagManager;
+    getProgram(): Program;
+    getReferencedFunction(address: Address): Function;
+    getReferencedVariable(instrAddr: Address, storageAddr: Address, size: number, isRead: boolean): Variable;
+    invalidateCache(all: boolean): void;
+    isInFunction(addr: Address): boolean;
+    moveAddressRange(fromAddr: Address, toAddr: Address, length: number, monitor: TaskMonitor): void;
+    removeFunction(entryPoint: Address): boolean;
 }
